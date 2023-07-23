4fc29df3fc629fdf6c11eb64578b9abffe228a8a
SaurabhSinghGithub <saurabhsinghgithu@gmail.com>
2023-07-23 15:23:06 +0530

    com

--- app/page.js
+++ app/page.js
@@ -11,16 +11,27 @@ export default function Home() {
       <h1 className='w-[70%] max-sm:w-[100%] flex justify-center items-center flex-col gap-2 rounded-full px-[40px] py-[15px] text-center text-[#ebe5e5]'>Explore & Disseminate
         <br className='sm:hidden' />
         <span className='typewriter mx-2 text-[#ffe29f]'>
-          ChatGPT Prompts
+          {/* ChatGPT Prompts */}
+          Pri Prompts
         </span>
         <div class="sprinkler text-center"></div>
       </h1>
 
-
+      {/* 
       <p className='max-w-4xl mx-auto text-center font-bold text-[#383838]'>
         <Typewriter
           options={{
             strings: ['PromptCraft is a AI-driven platform designed to explore engaging prompts.', 'Empowering individuals to unleash their creativity.', 'Share your imaginative ideas with the world.'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </p> */}
+
+      <p className='max-w-4xl mx-auto text-center font-bold text-[#383838]'>
+        <Typewriter
+          options={{
+            strings: ['Heyy Pri, go ahead and explore prompts. Hope you like it. Happy prompting!!!'],
             autoStart: true,
             loop: true,
           }}
